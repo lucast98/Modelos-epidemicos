bb1ec8d5efd6e30b045620902100d434a265c33d
Lucas Tavares <32081433+lucast98@users.noreply.github.com>
2020-06-25 20:48:03 -0300

    Add files via upload
    Comentarios

--- SI.py
+++ SI.py
@@ -2,23 +2,24 @@ from matplotlib import pyplot as plt
 import numpy as np
 import math
 
-delta_t = 0.25
-N = 100
-I = np.zeros(N)
-S = np.zeros(N)
-I[0] = 1
-S[0] = N-I[0]
-alfa = 2
+delta_t = 0.25 #intervalo de tempo entre valores
+N = 100 #tamanho da população
+I = np.zeros(N) #vetor com qtd de pessoas infectadas em função de n
+S = np.zeros(N) #vetor com qtd de pessoas suscetíveis em função de n
+I[0] = 1 #condição inicial
+S[0] = N-I[0] #condição inicial
+alfa = 2 #taxa de contato
 
+#Função para obter o resultado do modelo SI com uma única população
 def discreteSI_model():
     #print("0", S[0], I[0])
     produto = (alfa*delta_t)/N
     for n in range(len(S)-1):
         S[n+1] = S[n]*(1 - produto*I[n])
-        S[n+1] = math.floor(S[n+1])
+        S[n+1] = math.floor(S[n+1]) #arredonda para menos
 
         I[n+1] = I[n]*(1 + produto*S[n])
-        I[n+1] = math.ceil(I[n+1])
+        I[n+1] = math.ceil(I[n+1]) #arredonda par mais
         #print(n+1, S[n+1], I[n+1])
 
 if __name__ == '__main__':
